eb63b07a817b327070f8fbe54bfbe4299ed2c2f3
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-16 14:55:25 +0100

    feat: Add skeleton with frontend

---
+++ src/main/java/de/neuefische/springkanban/SpringKanbanApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.springkanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKanbanApplication.class, args);
+    }
+
+}
